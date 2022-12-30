659e51c6ba4cdcd69096cc206d8e9fa3771e4202
unknown <eliyahulevinson@gmail.com>
2022-12-30 20:41:37 +0200

    fixed db urls for tests

--- src/routes/categories/categories.test.js
+++ src/routes/categories/categories.test.js
@@ -7,8 +7,9 @@ const productsMongo = require('../../models/products/products.mongo');
 const app = require('../../app');
 
 const DB_URI =
-  process.env.MONGO_URI_TEST ||
-  'mongodb://localhost:27017/super-pharm-api-test';
+  process.env.ENV && process.env.ENV === TEST
+    ? process.env.MONGO_URI_TEST
+    : process.env.MONGO_URI || 'mongodb://localhost:27017/super-pharm-api-test';
 const API_URL = '/api/v1';
 
 describe('Testing categories API', () => {
